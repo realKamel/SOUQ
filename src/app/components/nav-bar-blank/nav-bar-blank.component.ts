9c7c11d1d4ca0a8a9714821ddd6f4e8b5b946501
abdelrahman <abdo.aly.k.m@gmail.com>
2025-01-02 03:24:14 +0200

    blankNavBarDesign

--- src/app/components/nav-bar-blank/nav-bar-blank.component.ts
+++ src/app/components/nav-bar-blank/nav-bar-blank.component.ts
@@ -1,10 +1,59 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+	Component,
+	CUSTOM_ELEMENTS_SCHEMA,
+	ElementRef,
+	HostListener,
+	output,
+	signal,
+	ViewChild,
+	WritableSignal,
+} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import {
+	LucideAngularModule,
+	Search,
+	Heart,
+	ShoppingCart,
+	CircleUserRound,
+} from 'lucide-angular';
+import { DividerModule } from 'primeng/divider';
+import { RouterLink } from '@angular/router';
+
 @Component({
 	selector: 'app-nav-bar-blank',
-	imports: [ButtonModule],
+	imports: [
+		ButtonModule,
+		ReactiveFormsModule,
+		FormsModule,
+		InputTextModule,
+		LucideAngularModule,
+		DividerModule,
+		RouterLink,
+	],
 	templateUrl: './nav-bar-blank.component.html',
 	styleUrl: './nav-bar-blank.component.scss',
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class NavBarBlankComponent {}
+export class NavBarBlankComponent {
+	@ViewChild('navbar') navbar!: ElementRef;
+	lucideIcons = {
+		Search: Search,
+		Heart: Heart,
+		ShoppingCart: ShoppingCart,
+		CircleUserRound: CircleUserRound,
+	};
+	q: WritableSignal<string> = signal('');
+	readonly isScrolled: WritableSignal<boolean> = signal(false);
+	private scrollMeasure = 0;
+	scrollToParent = output<number>();
+
+	@HostListener('window:scroll', [])
+	onWindowScroll() {
+		// Change navbar style after scrolling navbar height pixels
+		this.isScrolled.set(window.scrollY > this.scrollMeasure);
+		this.scrollMeasure = this.navbar.nativeElement.offsetHeight ?? 0;
+		this.scrollToParent.emit(this.scrollMeasure);
+	}
+}
--- src/app/layout/blank/blank.component.ts
+++ src/app/layout/blank/blank.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import {
+	AfterViewInit,
+	Component,
+	ElementRef,
+	HostListener,
+	inject,
+	PLATFORM_ID,
+	Renderer2,
+	ViewChild,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarBlankComponent } from '../../components/nav-bar-blank/nav-bar-blank.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
 	selector: 'app-blank',
@@ -9,4 +19,32 @@ import { FooterComponent } from '../../components/footer/footer.component';
 	templateUrl: './blank.component.html',
 	styleUrl: './blank.component.scss',
 })
-export class BlankComponent {}
+export class BlankComponent implements AfterViewInit {
+	scrollMeasure = 0;
+	@ViewChild('blankBody') blankBody!: ElementRef<HTMLDivElement>;
+	private readonly _Renderer2 = inject(Renderer2);
+	private readonly _PLATFORM_ID = inject(PLATFORM_ID);
+
+	@HostListener('window:scroll', [])
+	onWindowScroll() {
+		// Change navbar style after scrolling navbar height pixels
+		console.log(`${this.scrollMeasure}`);
+		if (isPlatformBrowser(this._PLATFORM_ID)) {
+			this._Renderer2.setStyle(
+				this.blankBody.nativeElement,
+				'marginTop',
+				`${this.scrollMeasure + (96 - 64)}px`
+			);
+		}
+	}
+	handleScrollChange(scroll: number) {
+		this.scrollMeasure = scroll;
+	}
+	ngAfterViewInit(): void {
+		this._Renderer2.setStyle(
+			this.blankBody.nativeElement,
+			'marginTop',
+			'100px'
+		);
+	}
+}
